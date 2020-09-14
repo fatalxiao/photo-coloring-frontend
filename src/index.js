818f8cf78032fc34103dfeb1f3e080d193d827a4
liangxiaojun <fatalxiao@163.com>
2020-09-14 16:34:36 +0800

    update src

--- src/index.js
+++ src/index.js
@@ -9,9 +9,50 @@ import $ from 'jquery';
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'scss/index.scss';
-import 'scss/container.scss';
-import 'scss/card.scss';
+import 'scss/Container.scss';
+import 'scss/Card.scss';
+import 'scss/CircularLoading.scss';
+
+/**
+ * 初始化
+ */
+function init() {
+    initTakeASelfieButton();
+    initUploadPhotoButton();
+}
+
+/**
+ * 初始化 Take A Selfie 按钮
+ */
+function initTakeASelfieButton() {
+    const takeASelfieButton = $('#source .btn-success');
+    takeASelfieButton.click(() => {
+        console.log(111);
+    });
+}
+
+/**
+ * 初始化 Upload Photo 按钮
+ */
+function initUploadPhotoButton() {
+    const uploadPhotoButton = $('#source .btn-info');
+    uploadPhotoButton.click(() => {
+
+        scrollToResultCard();
+
+    });
+}
+
+/**
+ * 滚动到 result card
+ */
+function scrollToResultCard() {
+    const resultCard = $('#result').show();
+    $('html, body').stop().animate({
+        scrollTop: resultCard.offset().top
+    }, 250);
+}
 
 $(() => {
-
+    init();
 });
