2c71a0e0a715046e15bdabb2d853bfd85a1a808a
liangxiaojun <fatalxiao@163.com>
2020-09-14 15:12:32 +0800

    update src

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * @file index.js
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+// Vendors
+import $ from 'jquery';
+
+// Styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'scss/index.scss';
+import 'scss/container.scss';
+import 'scss/card.scss';
+
+$(() => {
+
+});
